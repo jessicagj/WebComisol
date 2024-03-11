0f809e85edac8b005b3c1a70ca48c79c3d9d28f8
Oscar Cruz <ocruz@Raci.ti>
2024-03-11 13:03:58 -0600

    Se realizan los cambios en el backend de la pagina Contactanos se cambia el color en algunas partes del texto del cuerpo del correo, queda en espera el correo definitivo para el envio automatico del correo

--- WebComisol/Controllers/HomeController.cs
+++ WebComisol/Controllers/HomeController.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
+using System.Net.Mail;
+using System.Net;
 
 namespace WebComisol.Controllers
 {
@@ -43,6 +46,63 @@ namespace WebComisol.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contacto(string name, string phone, string email, string message)
+        {
+            try
+            {
+                if (message == "")
+                {
+                    message = "El cliente no escribio ningun mensaje, solo sus datos de comunicación, ponte en contacto lo mas pronto posible";
+                }
+
+                string messageBody = "<html><head><style> body {font-family: Candara; font-weight: 700; font-size: 1rem; margin: 0px;} .ff-serif {font-family: 'Open Sans' !important;font-weight: 600;}" +
+                        ".txt-bluelight {color: #10cfc9;} .txt-bluedark {color: #00a19b;}</style></head>" +
+                        "<body>" +
+                        "<div style='margin-left: 60px; margin-right: 60px; text-align: center;'>" +
+                        "<p style='color:#3b71ca;'>Estimado(a) Administrador(a):</p><p style='text-align: center;'> Se ha recibido una nueva solicitud de contacto desde nuestra web, los datos se describen a continuacion:<br>" +
+                        "<p text-align: start;> Nombre: " + name + " <br> Teléfono: " + phone + "<br>Correo Electronico: " + email + "<br>Mensaje: <br>" + message + "<br></p>" +
+                        "<p class='text-align:center' style='color:#3b71ca;'>Gracias por tu atención</p></div></body></html>";
+
+                string mesage;
+                string subject = "Solicitud de Contacto";
+                string from = "tecnologias@raciti.com.mx"; /*change After*/ /*changed*/
+                string to = "oscarc@raciti.com.mx"; /*change After*/ /*changed*/
+                MailMessage mail = new MailMessage();
+                mail.From = new MailAddress(from);
+                mail.To.Add(to);
+                mail.Subject = subject;
+                mail.IsBodyHtml = true;
+                mail.Body = messageBody;
+                mail.Priority = MailPriority.Normal;
+
+                //configureServerSMTP
+
+                //variables
+                string host = "smtp.office365.com";
+                string credential = from;
+                string passwordCredential = "Inicio.123";
+
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = host;
+                smtp.Port = 587;
+                smtp.EnableSsl = true;
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new NetworkCredential(credential, passwordCredential);
+                smtp.Send(mail);
+
+                return Json(new { mesage = "Datos enviados Correctamente" });
+            }
+
+            catch (Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+                ViewBag.Exception = ex;
+                return View("Error");
+
+            }
         }
     }
 }
---
+++ WebComisol/Scripts/Contacto.js
@@ -0,0 +1,66 @@
+﻿
+function send() {
+    let name = document.getElementById('inputName').value;
+    let phone = document.getElementById('inputPhone').value;
+    let email = document.getElementById('inputMail').value;
+    let message = document.getElementById('inputMessage').value;
+    const regexPhone = /^\d{10}$/;
+    const regexMail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?)*$/
+;
+
+    if (name != '') {
+        if (regexPhone.test(phone)) {
+            if (regexMail.test(email)) {
+                $.ajax({
+                    url: '/Home/Contacto',
+                    method: 'POST',
+                    data: {
+                        name: name,
+                        phone: phone,
+                        email: email,
+                        message: message
+                    },
+                    success: function (response) {
+                        Swal.fire({
+                            title: "Enviado",
+                            text: "Datos Enviados Correctamente",
+                            icon: "success"
+                        }).then((result)=> {
+                            if (result.isConfirmed) {
+                                window.location.href ="/Home/Index";
+                            }
+                        });
+                    },
+                    error: function (error) {
+                        Swal.fire({
+                            title: "Error",
+                            text: "A ocurrido un error",
+                            icon: "error"
+                        });
+                    },
+                });
+            }
+            else {
+                Swal.fire({
+                    title: "Error",
+                    text: "No es un formato de correo válido",
+                    icon: "error"
+                });
+            }
+        }
+        else {
+            Swal.fire({
+                title: "Error",
+                text: "No es un numero de teléfono válido, recuerda colocar a 10 digitos",
+                icon: "error"
+            });
+        }
+    }
+    else {
+        Swal.fire({
+            title: "Error",
+            text: "El campo nombre no puede estar vacío",
+            icon: "error"
+        });
+    }
+}
